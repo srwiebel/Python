8e38654557e5f91b0dd4eb5adafdbdcc52c700c8
srwiebel <srwiebel11@gmail.com>
2021-05-03 17:42:35 -0500

    ita programming

---
+++ Variables/variablesOutline.py
@@ -0,0 +1,56 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+a = 5 
+b = 4.5
+c = "paper cut" 
+d = True 
+e = ["apples","bananas","pears"] 
+print(type(b))
+
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+a = 7
+A= 10 
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+g = 6
+
+g = 3
+
+#4)
+#Make a variable and set it to a variable you have already created:
+bear = g
+
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+n = ["tomatoes","dog","mayo"] 
+k = n[1] 
+print(k)
+#6)
+#Make a String set to your name. Make a variable that is set to the first
+#character in that string:
+G = "sierra"
+l=G[0]
+print(l)
+#7)
+#Print out ONE of any of the variables you have created so far:
+print(A)
+
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+v = 13 
+print(V)
+
