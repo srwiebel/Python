d34c4c0d751b48679d0465d49ab73ef4a776998a
srwiebel <srwiebel11@gmail.com>
2021-05-10 14:50:25 -0500

    ita work

---
+++ Comments/commentsOutline.py
@@ -0,0 +1,25 @@
+'''
+This outline will help solidify concepts from the Comments lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a single line comment. 
+#Laura stinks 
+
+
+
+#2) Make a multi-line comment.
+
+'''
+I don't want to go to work today
+I just want to nap
+sleep sleep sleep
+'''
+
+
+
+#3) Make an int variable. Then, make a single line comment on the 
+#same line, after the variable.
+J = 7 # peaches 
+
+
