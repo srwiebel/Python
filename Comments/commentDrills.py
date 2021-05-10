c4de5a6d4042dbb98a54ee4ffd6d68c704c61f43
srwiebel <srwiebel11@gmail.com>
2021-05-10 14:56:20 -0500

    ita

---
+++ Comments/commentDrills.py
@@ -0,0 +1,49 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+#this is a single line comment
+
+
+
+#2) Make a multi-line comment.
+'''
+this is 
+a multi
+line comment
+'''
+
+
+#3) Make a variable. Then put a comment after it, on the same line.
+y = 8 # why doesn't equal eight 
+
+
+
+#4) Make a single line comment.
+# another single line comment 
+
+
+
+#5) Make a multi-line comment.
+'''
+multi 
+line
+comment
+muwahahahaha
+'''
+
+
