ac345f9769bcb1967e78620d6d51a26c822cabad
srwiebel <srwiebel11@gmail.com>
2021-05-13 17:38:16 -0500

    ita

---
+++ Input/inputDrills.py
@@ -0,0 +1,43 @@
+'''
+For this assignment you should read the task, then below the task do what it 
+asks you to do.
+
+EXAMPLE TASK:
+'''
+#EX) Get input from the user to the question "What is your favorite color?"
+#and store their answer in a variable called favColor.
+favColor = input("What is your favorite color?")
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Get input from the user to the question "What is your favorite animal?"
+#and store their answer in a variable called favAnimal.
+favAnimal = input("What is your favorite animal?")
+
+
+#2) Get input from the user to the question "What is your favorite number?"
+#and store their answer in a variable called favNum. Convert favNum from a 
+#string variable to a int variable.
+favNum = input("What is your favorite number?")
+favNum = int(favNum) 
+
+#3) Get input from the user to the question "What is your school's name?"
+#and store their answer in a variable called school.
+school = input("What is your school's name?")
+
+#4) Get input from the user to the question "What is your instructors name?"
+#and store their answer in a variable called instructor.
+instructor = input("What is your instructors name?")
+
+
+#5) Get input from the user to the question "What is 2 + 2?"
+#and store their answer in a variable called ans. Convert ans from a string
+#variable to a int variable
+ans = input("What is 2 + 2?")
+ans = int(ans)
